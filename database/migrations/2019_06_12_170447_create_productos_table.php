5d11d11e025bfdfc0037517f7fede6d43659a7ff
Luis Rodrigo Abasto <vida12345abasto@gmail.com>
2019-07-06 05:00:13 -0400

    ABASTO

---
+++ database/migrations/2019_06_12_170447_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idCategoria',11)->unsigned();
+            $table->string('nombre',30);
+            $table->string('descripcion',300);
+            $table->string('imagen',300);
+            $table->integer('stock',11);
+            $table->boolean('estado')->default(1);
+            $table->foreign('idCategoria')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
